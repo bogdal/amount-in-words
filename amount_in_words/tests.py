3957c3f02e6cd2af979ffe34fd405a361aff9028
Adam Bogdal <adam@bogdal.pl>
2013-01-11 21:31:41 +0100

    Improve readability of code

--- amount_in_words/tests.py
+++ amount_in_words/tests.py
@@ -6,15 +6,15 @@ import unittest
 class TestAmountInWords(unittest.TestCase):
 
     def test_convert_to_words(self):
-        items = [
+        cases = [
             ('0.94', u"dziewięćdziesiąt cztery grosze"),
             ('7092.12', u"siedem tysięcy dziewięćdziesiąt dwa złote i dwanaście groszy"),
             ('112301.01', u"sto dwanaście tysięcy trzysta jeden złotych i jeden grosz"),
             ('1000018', u"jeden milion osiemnaście złotych"),
             ('1.23', u"jeden złoty i dwadzieścia trzy grosze"),
         ]
-        for item in items:
-            self.assertEqual(Amount.in_words(item[0]), item[1])
+        for amount, words in cases:
+            self.assertEqual(Amount.in_words(amount), words)
 
     def test_to_big_amount(self):
         self.assertRaises(AmountError, Amount.in_words, Decimal(10**18))
